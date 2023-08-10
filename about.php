e4b96076cb776d46712c243bef0084747d2e7d42
AxitaGohel <141824930+AxitaGohel@users.noreply.github.com>
2023-08-09 23:57:14 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+        <title>About Us Page</title>
+
+        <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="maindiv2">
+
+    <?php
+        include("header.php");
+    ?>
+
+    <center><div class="h1">
+      <p>About Us</p>
+    </div></center>
+
+</div>
+ 
+<div class="container">
+    <div class="wrapper">
+        <img src="image/s1.jpg" alt="image1">
+        <img src="image/s2.jpg">
+        <img src="image/s3.jpg">
+        <img src="image/s4.jpg">
+    </div>
+</div>
+
+   <center> <p class="heading">How our company started</p></center>
+
+<div class="para1">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum rerum sequi optio reiciendis exercitationem asperiores necessitatibus alias voluptas voluptate molestiae? Ad nihil autem, in corrupti nemo id consectetur. Dolor, ab! Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam a minus repudiandae sequi dolorum est suscipit consequuntur atque perspiciatis nesciunt quod, praesentium odio eveniet. Pariatur corrupti beatae iusto recusandae veritatis!locale_filter_matches </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum rerum sequi optio reiciendis exercitationem asperiores necessitatibus alias voluptas voluptate molestiae? Ad nihil autem, in corrupti nemo id consectetur. Dolor, ab! Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam a minus repudiandae sequi dolorum est suscipit consequuntur atque perspiciatis nesciunt quod, praesentium odio eveniet. Pariatur corrupti beatae iusto recusandae veritatis!locale_filter_matches </p>
+</div>
+
+<?php
+        include("footer.php");
+    ?>
+        
+       
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
